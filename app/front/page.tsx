5e4c41bd0e21b5a8051f164a677490cd7796b085
Mohamed Esmail <div.mohamed.esmail@gmail.com>
2025-06-18 20:16:24 +0400

    Fix Error

---
+++ app/front/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function FrontPage() {
+  return (
+    <div>
+        <h2>Front Page Content</h2>
+        <p>This is the front page of the application.</p>
+    </div>
+  )
+}
+
+export default FrontPage
